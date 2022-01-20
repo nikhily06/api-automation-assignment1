cec05e28c083241b2f201a4b03c016dd2175167d
Nikhil Yadav <nikhilyadav@Nikhils-MacBook-Pro.local>
2022-01-20 21:03:02 +0530

    Added a test to create new user

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -14,6 +15,27 @@ public class SampleTest {
             .then()
                 .statusCode(200)
                 .log().body();
+    }
+
+    @Test
+    public void shouldCreateUser() {
+        // AAA
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer f783ed5aa518e9ec9e4e7febbf943956672c09233e3f819c1cf2fa2ba45a6a31")
+                .body("{\n" +
+                        "    \"name\":\"Tenali Ramakrishna\",\n" +
+                        "    \"gender\":\"male\",\n" +
+                        "    \"email\":\"tenali.ramakrishna1@anymail.com\",\n" +
+                        "    \"status\":\"active\"\n" +
+                        "}")
+            .when()
+                .post("https://gorest.co.in/public/v1/users")
+            .then()
+                .log().body()
+                .statusCode(201);
     }
 
 }
