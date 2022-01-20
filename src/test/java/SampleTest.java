4853d8e87089ef407d3135d265597b39ad480d9c
Nikhil Yadav <nikhilyadav@Nikhils-MacBook-Pro.local>
2022-01-20 21:20:48 +0530

    Added assertions to the response body

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,4 +1,5 @@
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -14,6 +15,8 @@ public class SampleTest {
                 .get("https://gorest.co.in/public/v1/users")
             .then()
                 .statusCode(200)
+                .body("data", Matchers.hasSize(20))
+                .body("data", Matchers.hasItem(Matchers.hasEntry("gender", "male")))
                 .log().body();
     }
 
@@ -28,14 +31,16 @@ public class SampleTest {
                 .body("{\n" +
                         "    \"name\":\"Tenali Ramakrishna\",\n" +
                         "    \"gender\":\"male\",\n" +
-                        "    \"email\":\"tenali.ramakrishna1@anymail.com\",\n" +
+                        "    \"email\":\"tenali.ramakrishna2@anymail.com\",\n" +
                         "    \"status\":\"active\"\n" +
                         "}")
             .when()
                 .post("https://gorest.co.in/public/v1/users")
             .then()
                 .log().body()
-                .statusCode(201);
+                .statusCode(201)
+                .body("data.id", Matchers.notNullValue())
+                .body("data.email", Matchers.equalTo("tenali.ramakrishna2@anymail.com"));
     }
 
 }
