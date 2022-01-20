373bc1ab6ba8577e4b5e3369bb4301afd07ead1b
Nikhil Yadav <nikhilyadav@Nikhils-MacBook-Pro.local>
2022-01-20 20:36:42 +0530

    Added first test invoking GetAllUsers API

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,11 +1,19 @@
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class SampleTest {
 
     @Test
-    public void shouldTestSomething() {
-        Assert.assertEquals(1, 1);
+    public void shouldGetAllUsers() {
+        // AAA
+
+        given()
+            .when()
+                .get("https://gorest.co.in/public/v1/users")
+            .then()
+                .statusCode(200)
+                .log().body();
     }
 
 }
